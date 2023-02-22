8978a78ef6108f69cd8046f2ee03a55fd2db74eb
HetPatel434 <119465121+HetPatel434@users.noreply.github.com>
2023-02-22 13:22:48 -0800

    Add files via upload

---
+++ INDEX.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PASSENGER</title>
+    <link rel="stylesheet" href="INDEX.CSS">
+</head>
+<body>
+<main>
+        <div class="container">
+        <div class="poster">
+            <img src="wallpaperflare-cropped (1).jpg" alt="movie-poster">
+         </div>
+
+         <div class="text-container">
+            <div class="text-1">
+                <h2>"THERE'S REASON WE WOKE UP EARLY"</h2>
+            </div>
+            <div class="text-2">
+                <h3>"IF YOU LIVE AN ORDINARY LIFE, ALL YOU HAVE ARE</h3>
+                <h3>ORDINARY STORIES"</h3>
+         </div>
+         </div>
+
+         <div class="actors">
+                <div class="actor1">
+                    <h2><a href="https://en.wikipedia.org/wiki/Jennifer_Lawrence">JENNIFER LAWRENCE</a></h2>
+                </div>
+                <div class="actor2">
+                    <h2><a href="https://en.wikipedia.org/wiki/Chris_Pratt">CHRIS PRATT</a></h2>
+               </div>
+         </div>
+
+
+               <div class="movie-name">
+                <div class="first">
+                    <h1><a href="https://www.imdb.com/title/tt1355644/?ref_=nv_sr_srsg_0">PASSENGER</a></h1>
+                </div>
+               </div>
+
+
+                <div class="line1">
+                    <p>COLUMBIA PICTURES</p>
+                    <P>LSTAR CAPITAL</P>
+                    <P>VILLAGE ROADSHOW PICTURES</P>
+                    <P>DECEMBER 21, 2016(United States) </P>                   
+                    
+                </div>            
+
+                
+            </div>
+        </div>
+    </div>
+
+
+
+            </main>
+</body>
+</html>
